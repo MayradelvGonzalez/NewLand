b99bf6d76ac193871cc66b7723f74ee8680c2e02
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2023-05-27 13:46:47 -0300

    iniciando

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get('/api', (req,res) => {
-    res.json('Hola desde el servidor')
+app.get('/api', (req, res) => {
+    res.json({ message: 'Hola desde el servidor' })
 })
 
 app.listen(PORT, ()=>{
