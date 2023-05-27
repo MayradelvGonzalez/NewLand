c821a786c82204a62c0544de3eedb06587a5f1a8
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2023-05-27 19:13:18 -0300

    AgregEstilosOpc

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,12 +3,13 @@ import './header.css';
 
 function Header() {
     return (
-     
+
         <div className="box">   <h2>Soy el Header</h2>
-        <ul className='lista'><li>Opciones</li>
-        <li>Opciones</li>
-        <li>Opciones</li></ul>
-        
+            <ul className='lista'>
+                <a href="/Info"><li>Info</li></a>
+                <a href="/proyectos"> <li>Opciones</li></a>
+                <a href="/contacto">  <li> Contacto</li></a>
+            </ul>
         </div>
     )
 }
