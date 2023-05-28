6f56f1bdbf6d726a6c807546aec4f365fb69d93b
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2023-05-27 23:02:08 -0300

    estilosBoots

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -4,13 +4,29 @@ import './header.css';
 function Header() {
     return (
 
-        <div className="box">   <h2>Soy el Header</h2>
-            <ul className='lista'>
-                <a href="/Info"><li>Info</li></a>
-                <a href="/proyectos"> <li>Opciones</li></a>
-                <a href="/contacto">  <li> Contacto</li></a>
-            </ul>
-        </div>
+        // <div className="box">   <h2>Soy el Header</h2>
+        //     <ul className='lista'>
+        //         <a href="/Info"><li>Info</li></a>
+        //         <a href="/proyectos"> <li>Opciones</li></a>
+        //         <a href="/contacto">  <li> Contacto</li><i class="bi bi-person-rolodex"></i></a>
+        //     </ul>
+        // </div>
+<div>
+<ul class="nav justify-content-center box">
+  {/* <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Portfolio</a>
+  </li> */}
+  <li class="nav-item">
+    <a class="nav-link" href="/">Info</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="/proyectos">Proyectos</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href='/contacto'>Contact</a><i class="bi bi-person-rolodex"></i>
+  </li>
+</ul>
+</div>
     )
 }
 export default Header;
--- client/src/components/Main/Main.jsx
+++ client/src/components/Main/Main.jsx
@@ -3,8 +3,10 @@ import './main.css';
 
 function Main() {
     return(
-<div className="box2">  <h2>Soy el main</h2></div>
-      
+<div className="box2">  <h2>Bienvenidos</h2>
+      <div><p>Hola 👋🏽 Soy <b>Mayra del Valle González</b></p></div>
+
+</div>
     )
 }
 
