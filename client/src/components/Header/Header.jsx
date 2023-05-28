3be56bc645c3586a81b0a7150c1980220f112217
MayradelvGonzalez <mayradelvallegonzalez@gmail.com>
2023-05-28 17:01:05 -0300

    agregElementos

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './header.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import avatar from '../../assets/images/avatar.png';
 
 function Header() {
     return (
 
         <div className='box'>
-            <div className='image'><img src="/client/src/img/avatar.png" alt="logo" /></div>
+            <div className='image'><img src={avatar} alt="logo" /></div>
             <ul class="nav justify-content-center lista">
 
                 <li class="nav-item">
